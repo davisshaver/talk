bdcbd30224e1d2aa971ef25654571670cf8e3e48
Davis Shaver <davisshaver@gmail.com>
2018-09-20 13:23:55 -0400

    Use un if available

--- plugins/talk-plugin-wp-signin/server/notfound.js
+++ plugins/talk-plugin-wp-signin/server/notfound.js
@@ -2,47 +2,54 @@ const UsersService = require('../../../services/users');
 const UserModel = require('../../../models/user');
 
 module.exports = {
-    tokenUserNotFound: async ({jwt, token}) => {
-        const username = await UsersService.getInitialUsername(
-            jwt.un
-        );
-        let user = await UserModel.findOneAndUpdate({
-            id: jwt.sub,
-        }, {
-            id: jwt.sub,
-            email: jwt.email,
-            username,
-            lowercaseUsername: username.toLowerCase(),
-            roles: [],
-            profiles: [{
-              provider: 'WordPress',
-              id: `${jwt.site}-${jwt.id}`,
-            },
-            {
-               provider: "local",
-               id: jwt.email,
-               username: jwt.un
-            }],
-            provider: 'local',  
-            status: {
-                username: {
-                    status: 'SET',
-                    history: [{
-                        status: 'SET',
-                    }],
-                }
-            },
-            metadata: {
-                displayName: jwt.un,
-            },
-        }, {
-            setDefaultsOnInsert: true,
-            new: true,
-            upsert: true,
-        });
-        if (!user) {
-            return null;
-        }
-        return user;
+  tokenUserNotFound: async ({ jwt }) => {
+    const username = await UsersService.getInitialUsername(jwt.un);
+    let user = await UserModel.findOneAndUpdate(
+      {
+        id: jwt.sub,
+      },
+      {
+        id: jwt.sub,
+        email: jwt.email,
+        username,
+        lowercaseUsername: username.toLowerCase(),
+        roles: [],
+        profiles: [
+          {
+            provider: 'WordPress',
+            id: `${jwt.site}-${jwt.id}`,
+            username: jwt.un,
+          },
+          {
+            provider: 'local',
+            id: jwt.email,
+            username: jwt.un,
+          },
+        ],
+        provider: 'local',
+        status: {
+          username: {
+            status: 'SET',
+            history: [
+              {
+                status: 'SET',
+              },
+            ],
+          },
+        },
+        metadata: {
+          displayName: jwt.un,
+        },
+      },
+      {
+        setDefaultsOnInsert: true,
+        new: true,
+        upsert: true,
+      }
+    );
+    if (!user) {
+      return null;
     }
+    return user;
+  },
 };
